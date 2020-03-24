8942cdcd91e850706c7ea1c1d83752fb0d5aeceb
aizwellenstan <aizwellenstan@gmail.com>
2020-03-24 20:11:45 +0900

    startup

---
+++ client.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using System.Collections;
+using WebSocketSharp;
+using WebSocketSharp.Net;
+
+public class ClientExample: MonoBehaviour {
+
+    WebSocket ws;
+
+    void Start()
+    {
+        ws = new WebSocket("ws://localhost:3000/");
+
+        ws.OnOpen += (sender, e) =>
+        {
+            Debug.Log("WebSocket Open");
+        };
+
+        ws.OnMessage += (sender, e) =>
+        {
+            Debug.Log("WebSocket Message Type: " + e.Type + ", Data: " + e.Data);
+        };
+
+        ws.OnError += (sender, e) =>
+        {
+            Debug.Log("WebSocket Error Message: " + e.Message);
+        };
+
+        ws.OnClose += (sender, e) =>
+        {
+            Debug.Log("WebSocket Close");
+        };
+
+        ws.Connect();
+
+    }
+
+    void Update()
+    {
+
+        if (Input.GetKeyUp("s"))
+        {
+            ws.Send("Test Message");
+        }
+
+    }
+
+    void OnDestroy()
+    {
+        ws.Close();
+        ws = null;
+    }
+}
